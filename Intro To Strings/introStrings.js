2457b45aec0010c5d1970af0d2532720674acb66
adityajkate <adityakate11@gmail.com>
2026-03-12 23:47:10 +0530

    Basics of String

---
+++ Intro To Strings/introStrings.js
@@ -0,0 +1,19 @@
+let str = "Aditya";
+
+// Basic of Strings of Js
+console.log(str.length);
+
+// Basic of Substring and returns non-negetive indices
+console.log(str.substring(1, 5));
+console.log(str.substring(1));
+
+// Basics of Slice and accepts negetive indices
+console.log(str.slice(1, 6));
+console.log(str.slice(-3, -1));
+
+// Lowercase and uppercase
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+
+// concat
+console.log(str.concat(" hi", " this is me."));
