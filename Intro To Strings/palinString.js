f8d3f9563ca906f2cc00628358b7633faf15ef2c
adityajkate <adityakate11@gmail.com>
2026-03-13 20:05:25 +0530

    Palindrome Method 2

--- Intro To Strings/palinString.js
+++ Intro To Strings/palinString.js
@@ -1,12 +1,29 @@
-let str = "Aditya";
-let rev = "";
+// Method 1
+let str = "hello";
+// let rev = "";
 
-for (let i = str.length - 1; i >= 0; i--) {
-  rev = rev + str[i];
-}
+// for (let i = str.length - 1; i >= 0; i--) {
+//   rev = rev + str[i];
+// }
 
-if (str == rev) {
-  console.log("Strings are Palindrome!");
-} else {
-  console.log("Strings are not Palindrome!");
+// if (str == rev) {
+//   console.log("Strings are Palindrome!");
+// } else {
+//   console.log("Strings are not Palindrome!");
+// }
+
+// Method 2
+let i = 0,
+  j = str.length - 1;
+let isPalindrome = true;
+while (i < j) {
+  if (str[i] != str[j]) {
+    isPalindrome = false;
+    break;
+  }
+  i++;
+  j--;
 }
+isPalindrome
+  ? console.log("String is Palindrome!")
+  : console.log("String is not Palindrome!");
