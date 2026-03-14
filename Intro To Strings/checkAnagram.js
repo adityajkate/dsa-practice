9e2749308a2ca2309e75a47135eb98541c82454e
adityajkate <adityakate11@gmail.com>
2026-03-14 20:23:34 +0530

    check whether both strings are anagrams or not

---
+++ Intro To Strings/checkAnagram.js
@@ -0,0 +1,24 @@
+let s1 = "arc";
+let s2 = "car";
+let isAnagram = true;
+
+let freqArr = new Array(123).fill(0);
+if (s1.length != s2.length) {
+  isAnagram = false;
+} else {
+  for (let i = 0; i < s1.length; i++) {
+    let ascii = s1.charCodeAt(i);
+    freqArr[ascii] += 1;
+  }
+  for (let i = 0; i < s2.length; i++) {
+    let ascii = s2.charCodeAt(i);
+    freqArr[ascii] -= 1;
+  }
+  for (let i = 0; i < freqArr.length; i++) {
+    if (freqArr[i] != 0) {
+      isAnagram = false;
+    }
+  }
+}
+
+isAnagram ? console.log("Anagrams!") : console.log("Not Anagrams!");
