79041a48759e49f72224d277f35f9a513877f366
adityajkate <adityakate11@gmail.com>
2026-03-16 19:55:51 +0530

    Happy Number

---
+++ Intro to Set/happyNumber.js
@@ -0,0 +1,21 @@
+// leetcode - https://leetcode.com/problems/happy-number/
+
+var isHappy = function (n) {
+  let set = new Set();
+
+  while (true) {
+    let sum = 0;
+
+    while (n > 0) {
+      let rem = n % 10;
+      sum += rem * rem;
+      n = Math.floor(n / 10);
+    }
+
+    if (sum === 1) return true;
+    if (set.has(sum)) return false;
+
+    set.add(sum);
+    n = sum;
+  }
+};
