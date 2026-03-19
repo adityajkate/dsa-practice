304859682a20ad9a1165fd7a3029c7a41c491052
adityajkate <adityakate11@gmail.com>
2026-03-19 13:42:49 +0530

    Swapping of two variables

---
+++ Intro to Bitwise/swap2Var.js
@@ -0,0 +1,9 @@
+// Swapping of 2 variables using Exor
+let a = 10;
+let b = 20;
+console.log(`Before Swapping: ${a} ${b}`);
+
+a = a ^ b;
+b = a ^ b;
+a = a ^ b;
+console.log(`After Swapping: ${a} ${b}`);
