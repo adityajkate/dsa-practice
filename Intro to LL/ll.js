837297f6925bb4168a5d31974e036ecc2147bcbf
adityajkate <adityakate11@gmail.com>
2026-05-05 13:44:24 +0530

    feat: Intro to Linked List

---
+++ Intro to LL/ll.js
@@ -0,0 +1,41 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  InsertAtFirst(val) {
+    this.size++;
+    let newNode = new Node(val);
+    if (this.head == null) {
+      this.head = newNode;
+      return;
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  printLinkedList() {
+    let temp = this.head;
+    while (temp !== null) {
+      process.stdout.write(temp.val + "->");
+      temp = temp.next;
+    }
+    console.log("null");
+  }
+}
+
+let obj = new LinkedList();
+obj.InsertAtFirst(10);
+obj.InsertAtFirst(20);
+obj.InsertAtFirst(30);
+obj.InsertAtFirst(40);
+
+obj.printLinkedList();
