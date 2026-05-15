73464a8768d65df6ec5cbe44df736f15fe5f8ea5
adityajkate <adityakate11@gmail.com>
2026-05-15 19:48:35 +0530

    feat: Perfect Number Problem SimpleSolution

---
+++ Basic Problems/perfectNumber.js
@@ -0,0 +1,19 @@
+// Leetcode link : https://leetcode.com/problems/perfect-number/
+
+var checkPerfectNumber = function (num) {
+  if (num <= 1) return false;
+
+  let sum = 1;
+
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0) {
+      sum += i;
+
+      if (i !== num / i) {
+        sum += num / i;
+      }
+    }
+  }
+
+  return sum === num;
+};
